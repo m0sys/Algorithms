81324825827ac776d45844d79f4aa75a4ad5af11
mhd53 <mhd53@users.noreply.github.com>
2020-07-24 05:43:49 -0400

    update

---
+++ MasterLangC/code-for-fun/bit.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    if (8 & 7) {
+        printf("true\n");
+    }
+    else {
+        printf("false\n");
+    }
+
+    printf("%d\n", 8  >> 1);
+    return 0;
+}
